ddf1669286cf4b426399663c9f1af9349efdc320
Melicious <126588135+hviet03@users.noreply.github.com>
2023-06-07 14:00:58 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,72 @@
+<!--footer-->
+<div class="footer">
+    <!-- container -->
+    <div class="container">
+      <div class="col-md-6 footer-left">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="teacher/login.php">Teacher</a></li>
+          <li><a href="student/login.php">Student</a></li>
+        </ul>
+       
+      </div>
+      <div class="col-md-3 footer-middle">
+        <?php
+$sql="SELECT * from tblpage where PageType='contactus'";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $row)
+{               ?>
+        <h3>Address</h3>
+        <div class="address">
+          <p><?php  echo htmlentities($row->PageDescription);?>
+          </p>
+        </div>
+        <div class="phone">
+          <p><?php  echo htmlentities($row->MobileNumber);?></p>
+        </div>
+      <?php $cnt=$cnt+1;}} ?></div>
+      <div class="col-md-3 footer-right">
+        <h3>EHC</h3>
+        <p>Chơi CTF đê</p>
+      </div>
+      <div class="clearfix"> </div> 
+    </div>
+    <!-- //container -->
+  </div>
+<!--/footer-->
+<!--copy-rights-->
+<div class="copyright">
+    <!-- container -->
+    <div class="container">
+      <div class="copyright-left">
+      <p>© <?php echo date('Y');?> Student Management</p>
+      </div>
+      <div class="copyright-right">
+      </div>
+      <div class="clearfix"> </div>
+      
+    </div>
+    <!-- //container -->
+    <!---->
+<script type="text/javascript">
+    $(document).ready(function() {
+        /*
+        var defaults = {
+        containerID: 'toTop', // fading element id
+        containerHoverID: 'toTopHover', // fading element hover id
+        scrollSpeed: 1200,
+        easingType: 'linear' 
+        };
+        */
+    $().UItoTop({ easingType: 'easeOutQuart' });
+});
+</script>
+<a href="#to-top" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!----> 
+  </div>
